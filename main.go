ef628b0ecbc122e9ff678eb3ad4a94be468e9e59
rizkyrajitha <rajithagunathilake@gmail.com>
2021-08-19 14:07:26 +0530

    homepage added , now exit if the broker address is not specified

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	collector "rizkyrajitha.github.io/mosquitto_exporter/collector"
 	mqttcon "rizkyrajitha.github.io/mosquitto_exporter/mqttcon"
 )
 
@@ -17,27 +18,35 @@ var version, brokerAddress, username, password, listenPort string
 func main() {
 
 	flag.Usage = func() {
-		fmt.Printf("mosquitto_exporter %s\nA Mosquitto Mqtt Broker metric exporter for Prometheus\n\nOptions\n", version)
-        flag.PrintDefaults()
+		fmt.Printf("mosquitto_exporter v%s\nA Mosquitto Mqtt Broker metric exporter for Prometheus\n\nOptions\n", version)
+		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&brokerAddress, "brokerAddress", "127.0.0.1:1883", "mqtt broker ip / url and port")
+	flag.StringVar(&brokerAddress, "brokerAddress", "", "mqtt broker ip / url and port. ex -127.0.0.1:1883 / broker.yourdomain.com:1883")
 	flag.StringVar(&listenPort, "listenPort", "9992", "exporter listening address")
 	flag.StringVar(&username, "username", "", "mqtt broker username")
 	flag.StringVar(&password, "password", "", "mqtt password username")
 
-	if len(os.Args) < 2 {
-		log.Fatal("expected brokerAddress")
-	}
-
 	flag.Parse()
 
+	if brokerAddress == "" {
+		fmt.Println("Required brokerAddress ex - 127.0.0.1:1883 / broker.yourdomain.com:1883")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	log.Println("brokerAddress : ", brokerAddress)
+
 	if username != "" {
 		log.Println("username : ", username)
 	}
 
-	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/", homePageHandler)
+
+	registry := collector.Registry()
+	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+
+	http.Handle("/metrics", handler)
 
 	log.Println("mosquitto_exporter listening on port", listenPort)
 
@@ -48,4 +57,23 @@ func main() {
 		log.Fatal("Error listening on port : ", listenPort)
 	}
 
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+
+	homePageHtml := `
+		<html> 
+		
+		<head>	
+		<title> mosquitto_exporter v` + version + `</title>
+		</head>	
+		<body>
+		
+		<h1>mosquitto_exporter v` + version + `</h1> 
+		<a href="/metrics" >metrics</a> 
+		
+		</body>
+		</html>`
+
+	fmt.Fprint(w, homePageHtml)
 }
